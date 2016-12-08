933da44694e8875089a7ed107eb56089e3b8714e
eroshenkovmaksim <https://eroshenkovmaksim@bitbucket.org/eroshenkovmaksim/almaz.git>
2016-12-08 21:10:59 +0300

    more web

--- src/main/java/com/ncblog/web/WebBlog.java
+++ src/main/java/com/ncblog/web/WebBlog.java
@@ -9,8 +9,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.ncblog.dataaccess.repositories.CommentRepository;
 import com.ncblog.dataaccess.repositories.PostRepository;
 import com.ncblog.dataaccess.repositories.UserRepository;
+import com.ncblog.domain.Comment;
 import com.ncblog.domain.Post;
 import com.ncblog.domain.User;
 import spark.ModelAndView;
@@ -20,23 +22,24 @@ import spark.Spark;
 import static spark.Spark.*;
 
 public class WebBlog {
-
+    static Map<String,User> userSessions = new HashMap<>();
+    static Map<String,Post> postSessions = new HashMap<>();
     public static void main(String args[]) {
         UserRepository userRepository = new UserRepository();
         PostRepository postRepository = new PostRepository();
+        CommentRepository commentRepository = new CommentRepository();
+
         get("/status", ((request, response) -> {
             HashMap<Object, Object> model = new HashMap<>();
             return new ModelAndView(model, "login.html");
         }), new FreeMarkerEngine());
+
         get("/registration", ((request, response) -> {
             HashMap<Object, Object> model = new HashMap<>();
             return new ModelAndView(model, "registration.html");
         }), new FreeMarkerEngine());
 
-//        get("/personalpage", ((request, response) -> {
-//            HashMap<Object, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "registration.html");
-//        }), new FreeMarkerEngine());
+
 
         post("/registration", (request, response) -> {
             String login = request.queryParams("login");
@@ -53,22 +56,13 @@ public class WebBlog {
         post("/login", (request, response) -> {
             String login = request.queryParams("login");
             String password = request.queryParams("password");
-            Map<String, Object> attributes = new HashMap<>();
             User user =userRepository.getUserWithLogin(login);
             String value = request.queryParams("button");
             if("Sign In".equalsIgnoreCase(value)){
                 if (user != null && (user.getPassword()).equals(password)) {
-                    List<String> contents = new ArrayList<String>();
-                    List<Post> posts = postRepository.getUserPosts(login);
-                    for (Post post : posts) {
-                        contents.add(post.getContent());
-                    }
-                    attributes.put("login", login);
-                    attributes.put("contents", contents);
-//                attributes.put("date", posts);
-//                    response.body("123");
-//                    response.redirect("/personalpage");
-                    return new ModelAndView(attributes, "personalPage.html");
+                    userSessions.put(request.session().id(),user);
+                    response.redirect("/personalpage");
+                    return null;
                 } else {
                     System.out.println("wrong password");
                     HashMap<Object, Object> model = new HashMap<>();
@@ -79,6 +73,54 @@ public class WebBlog {
                 return null;
             }
         },new FreeMarkerEngine());
+
+        get("/personalpage", ((request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            String currentSession = request.session().id();
+            if(userSessions.containsKey(currentSession)){
+                User user = userSessions.get(currentSession);
+                String login = user.getLogin();
+                List<Post> posts = postRepository.getUserPosts(login);
+                attributes.put("login", login);
+                attributes.put("posts", posts);
+                return new ModelAndView(attributes, "personalPage.html");
+            }
+            response.redirect("/login");
+            return null;
+        }), new FreeMarkerEngine());
+
+        post("/personalpage", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            String currentSession = request.session().id();
+            System.out.println(currentSession);
+            if(userSessions.containsKey(currentSession)){
+                User user = userSessions.get(currentSession);
+                String login = user.getLogin();
+                List<Post> posts = postRepository.getUserPosts(login);
+                for(Post post : posts){
+                    String value = request.queryParams(post.getContent());
+
+                    if("Comments".equalsIgnoreCase(value)){
+                        postSessions.put(currentSession,post);
+                        response.redirect("/comments");
+                    }
+                }
+            }
+            return null;
+        },new FreeMarkerEngine());
+
+        get("/comments", ((request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            String currentSession = request.session().id();
+            if(postSessions.containsKey(currentSession)){
+                Post post = postSessions.get(currentSession);
+                List<Comment> comments = commentRepository.getPostComments(post);
+                attributes.put("post", post.getContent());
+                attributes.put("comments", comments);
+                return new ModelAndView(attributes, "comments.html");
+            }
+            return null;
+        }), new FreeMarkerEngine());
     }
 
 }
